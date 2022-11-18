c4ebf2e42d93bc317b7581cad4efee585056b518
Gavin (Zhengyu) Yang <yang.zhengy@northeastern.edu>
2022-11-18 09:08:43 -0800

    Save the changes

--- app/src/main/java/edu/northeastern/elderberry/LoginActivity.java
+++ app/src/main/java/edu/northeastern/elderberry/LoginActivity.java
@@ -7,21 +7,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private String username;
     private String password;
+    private DatabaseReference userDatabase;
+    private final ArrayList<DisplayUsername> listOfUsers = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +68,26 @@ public class LoginActivity extends AppCompatActivity {
             Log.d(TAG, "_____onClick");
             startMedicationTrackerActivity();
         });
+
+        // Init the database
+        this.userDatabase = FirebaseDatabase.getInstance().getReference("users");
+
+        this.userDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d(TAG, "_____onDataChange");
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    listOfUsers.add(new DisplayUsername((String) dataSnapshot.child("username").getValue()));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d(TAG, "_____onCancelled");
+                Toast.makeText(LoginActivity.this, "Error: Unable to get access to Database", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     private void startMedicationTrackerActivity() {
@@ -72,11 +108,13 @@ public class LoginActivity extends AppCompatActivity {
             this.username = enterUsername.getText().toString();
             this.password = enterPassword.getText().toString();
 
+
             if (isInvalid(this.username) || isInvalid(this.password)) {
                 Snackbar.make(view, "Unsuccessful account creation!", Snackbar.LENGTH_SHORT).show();
             } else {
                 Snackbar.make(view, "Successful account creation!", Snackbar.LENGTH_SHORT).show();
             }
+            // createNewUser(view);
             dialog.dismiss();
         });
 
@@ -120,6 +158,43 @@ public class LoginActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    public void createNewUser(View view) {
+        Log.d(TAG, "_____createNewUser");
+        // Todo add password to the database
+        // Todo check for duplication username adds
+        EditText newUserName = findViewById(R.id.enterUsername);
+        boolean duplicateUsername = false;
+        Toast.makeText(LoginActivity.this,"CreateNewUser clicked", Toast.LENGTH_SHORT).show();
+
+        if (newUserName.getText().toString().equals("") || newUserName.getText().toString().isBlank()) {
+            Toast.makeText(LoginActivity.this, "Your username must include at least 1 non-white space character!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // check for duplicates in the database
+        for (DisplayUsername username : listOfUsers) {
+            if (username.getUsername().equals(newUserName.getText().toString())) {
+                duplicateUsername = true;
+                break;
+            }
+        }
+
+        if (!duplicateUsername) {
+            Map<String, String> newUser = new HashMap<>();
+            newUser.put("username", newUserName.getText().toString());
+            Task<Void> task = this.userDatabase.push().setValue(newUser);
+
+            try {
+                task.addOnSuccessListener(o -> finish());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(LoginActivity.this, "Sorry! This username already exists. Please create a new one!", Toast.LENGTH_LONG).show();
         }
     }
 }
