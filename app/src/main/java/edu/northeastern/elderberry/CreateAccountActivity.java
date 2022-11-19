b913e97869d5abb1e54602bab7e1606ec9939cb1
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2022-11-18 19:30:35 -0500

    Reverting to 5841c275e597cd87f1435a242ebc8d044bd69693

---
+++ app/src/main/java/edu/northeastern/elderberry/CreateAccountActivity.java
@@ -0,0 +1,116 @@
+package edu.northeastern.elderberry;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    private static final String TAG = "CreateAccountActivity";
+    private FirebaseAuth mAuth;
+    private String firstName;
+    private String lastName;
+    private String registeredEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "_____onCreate");
+        setContentView(R.layout.activity_create_account);
+
+        // Initialize Firebase Auth.
+        // Get the shared instance of the FirebaseAuth object.
+        this.mAuth = FirebaseAuth.getInstance();
+        // Allows us to check if the user is already authenticated or not. If the user is already authenticated we will close the activity.
+        if (this.mAuth.getCurrentUser() != null) {
+            finish();
+            return;
+        }
+
+        // Calling this activity's function to use ActionBar utility methods.
+        ActionBar actionBar = getSupportActionBar();
+
+        // Providing a subtitle for the ActionBar.
+        assert actionBar != null;
+        actionBar.setSubtitle(getString(R.string.medication_tracker));
+
+        // Adding an icon in the ActionBar.
+        actionBar.setIcon(R.mipmap.app_logo);
+
+        // Methods to display the icon in the ActionBar.
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        // Create Account Functionality.
+        Button createAccountButton = findViewById(R.id.createAccount);
+        createAccountButton.setOnClickListener(v -> {
+            Log.d(TAG, "_____onCreate");
+            registerUser();
+        });
+    }
+
+    // When initializing the activity, check to see if the user is currently signed in.
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            currentUser.reload();
+        }
+    }
+
+    private void registerUser() {
+        Log.d(TAG, "_____registerUser");
+        EditText etFirstName = findViewById(R.id.et_firstName);
+        EditText etLastName = findViewById(R.id.et_lastName);
+        EditText etRegisteredEmail = findViewById(R.id.et_email);
+        EditText etRegisteredPassword = findViewById(R.id.et_password);
+        this.firstName = etFirstName.getText().toString();
+        this.lastName = etLastName.getText().toString();
+        this.registeredEmail = etRegisteredEmail.getText().toString();
+        String registeredPassword = etRegisteredPassword.getText().toString();
+
+        if (isInvalid(this.firstName) || isInvalid(this.lastName) || isInvalid(this.registeredEmail) || isInvalid(registeredPassword)) {
+            Toast.makeText(this, "Unsuccessful account creation! Please fill all fields.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        this.mAuth.createUserWithEmailAndPassword(this.registeredEmail, registeredPassword)
+                .addOnCompleteListener(this, task -> {
+                    Log.d(TAG, "_____onComplete");
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "_____onComplete (signInWithEmail:success)");
+                        User user = new User(firstName, lastName, registeredEmail);
+                        FirebaseDatabase.getInstance().getReference("users")
+                                .child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                                .setValue(user).addOnCompleteListener(task1 -> showMedicationTrackerActivity());
+                    } else {
+                        Log.w(TAG, "_____onComplete (signInWithEmail:failure)", task.getException());
+                        Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void showMedicationTrackerActivity() {
+        Log.d(TAG, "_____startMedicationTrackerActivity");
+        Intent intent = new Intent(this, MedicationTrackerActivity.class);
+        startActivity(intent);
+    }
+
+    private static boolean isInvalid(String s) {
+        Log.d(TAG, "_____isValid");
+        return s == null || s.isBlank() || s.isEmpty();
+    }
+}
