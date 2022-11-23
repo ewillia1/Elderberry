af6865479663762e625dea7b66bac0056bb48ab2
Gavin (Zhengyu) Yang <yang.zhengy@northeastern.edu>
2022-11-23 10:27:30 -0800

    New dayview using recycler view partial complete

---
+++ app/src/main/java/edu/northeastern/elderberry/dayview/ChildItemAdapter.java
@@ -0,0 +1,94 @@
+package edu.northeastern.elderberry.dayview;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import edu.northeastern.elderberry.R;
+
+public class ChildItemAdapter
+        extends RecyclerView
+        .Adapter<ChildItemAdapter.ChildViewHolder> {
+
+    private List<ChildItem> ChildItemList;
+
+    // Constructor
+    ChildItemAdapter(List<ChildItem> childItemList)
+    {
+        this.ChildItemList = childItemList;
+    }
+
+    @NonNull
+    @Override
+    public ChildViewHolder onCreateViewHolder(
+            @NonNull ViewGroup viewGroup,
+            int i)
+    {
+
+        // Here we inflate the corresponding
+        // layout of the child item
+        View view = LayoutInflater
+                .from(viewGroup.getContext())
+                .inflate(
+                        R.layout.child_item,
+                        viewGroup, false);
+
+        return new ChildViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(
+            @NonNull ChildViewHolder childViewHolder,
+            int position)
+    {
+
+        // Create an instance of the ChildItem
+        // class for the given position
+        ChildItem childItem
+                = ChildItemList.get(position);
+
+        // For the created instance, set title.
+        // No need to set the image for
+        // the ImageViews because we have
+        // provided the source for the images
+        // in the layout file itself
+        childViewHolder
+                .ChildItemTitle
+                .setText(childItem.getChildItemTitle());
+    }
+
+    @Override
+    public int getItemCount()
+    {
+
+        // This method returns the number
+        // of items we have added
+        // in the ChildItemList
+        // i.e. the number of instances
+        // of the ChildItemList
+        // that have been created
+        return ChildItemList.size();
+    }
+
+    // This class is to initialize
+    // the Views present
+    // in the child RecyclerView
+    class ChildViewHolder
+            extends RecyclerView.ViewHolder {
+
+        TextView ChildItemTitle;
+
+        ChildViewHolder(View itemView)
+        {
+            super(itemView);
+            ChildItemTitle
+                    = itemView.findViewById(
+                    R.id.child_item_title);
+        }
+    }
+}
---
+++ app/src/main/java/edu/northeastern/elderberry/dayview/MedicationDayview.java
@@ -0,0 +1,158 @@
+package edu.northeastern.elderberry.dayview;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.northeastern.elderberry.R;
+
+public class MedicationDayview extends AppCompatActivity {
+    private static final String TAG = "MedicationDayViewActivity";
+
+    ImageButton arrow;
+    LinearLayout hiddenView;
+    CardView cardView;
+    private DatabaseReference userDB;
+    private List<String> medicines = new ArrayList<>();
+
+    // Todo show the data of the medication
+    // Todo add database reference
+    // Todo add checkbox to the collapsable card view
+    // Todo checkbox should result in data updated somewhere
+    // Todo get user id to this view for db query
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_medication_dayview);
+        setContentView(R.layout.activity_recycle_med_dayview);
+        // setting up db
+        this.userDB = FirebaseDatabase.getInstance().getReference();
+        RecyclerView
+                ParentRecyclerViewItem
+                = findViewById(
+                R.id.parent_recyclerview);
+
+        // Initialise the Linear layout manager
+        LinearLayoutManager
+                layoutManager
+                = new LinearLayoutManager(
+                MedicationDayview .this);
+
+        // Pass the arguments
+        // to the parentItemAdapter.
+        // These arguments are passed
+        // using a method ParentItemList()
+        ParentItemAdapter
+                parentItemAdapter
+                = new ParentItemAdapter(
+                ParentItemList());
+
+        // Set the layout manager
+        // and adapter for items
+        // of the parent recyclerview
+        ParentRecyclerViewItem
+                .setAdapter(parentItemAdapter);
+        ParentRecyclerViewItem
+                .setLayoutManager(layoutManager);
+    }
+
+    private List<ParentItem> ParentItemList()
+    {
+        List<ParentItem> itemList
+                = new ArrayList<>();
+
+        ParentItem item
+                = new ParentItem(
+                "Title 1",
+                ChildItemList());
+        itemList.add(item);
+        ParentItem item1
+                = new ParentItem(
+                "Title 2",
+                ChildItemList());
+        itemList.add(item1);
+        ParentItem item2
+                = new ParentItem(
+                "Title 3",
+                ChildItemList());
+        itemList.add(item2);
+        ParentItem item3
+                = new ParentItem(
+                "Title 4",
+                ChildItemList());
+        itemList.add(item3);
+
+        return itemList;
+    }
+
+    // Method to pass the arguments
+    // for the elements
+    // of child RecyclerView
+    private List<ChildItem> ChildItemList()
+    {
+        List<ChildItem> ChildItemList
+                = new ArrayList<>();
+
+        ChildItemList.add(new ChildItem("Card 1"));
+        ChildItemList.add(new ChildItem("Card 2"));
+        ChildItemList.add(new ChildItem("Card 3"));
+        ChildItemList.add(new ChildItem("Card 4"));
+
+        return ChildItemList;
+
+        // setting up cardView
+        //cardView = findViewById(R.id.base_cardview);
+        //arrow = findViewById(R.id.arrow_button);
+        //hiddenView = findViewById(R.id.hidden_view);
+
+        //arrow.setOnClickListener(view -> {
+        //    // If the CardView is already expanded, set its visibility
+        //    // to gone and change the expand less icon to expand more.
+        //    if (hiddenView.getVisibility() == View.VISIBLE) {
+        //        // The transition of the hiddenView is carried out by the TransitionManager class.
+        //        // Here we use an object of the AutoTransition Class to create a default transition
+        //        TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+        //        hiddenView.setVisibility(View.GONE);
+        //        arrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
+        //    }
+
+        //    // If the CardView is not expanded, set its visibility to
+        //    // visible and change the expand more icon to expand less.
+        //    else {
+        //        TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+        //        hiddenView.setVisibility(View.VISIBLE);
+        //    }
+        //});
+
+        //String user = "Gavin";
+        //this.userDB.child(user).addValueEventListener(new ValueEventListener() {
+        //    @Override
+        //    public void onDataChange(@NonNull DataSnapshot snapshot) {
+        //        medicines.add(snapshot.getValue(Medicine.class).getName());
+        //        Log.d(TAG, "onDataChange: " + "med is " + medicines.get(0));
+        //        // for (DataSnapshot d: snapshot.getChildren()) {
+        //        //    //medicines.add();
+        //        //}
+        //    }
+
+        //    @Override
+        //    public void onCancelled(@NonNull DatabaseError error) {
+
+        //    }
+        //});
+    }
+}
