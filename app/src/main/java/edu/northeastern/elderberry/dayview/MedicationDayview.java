c9138dedb528fdbdb9890b8f3ce0d6378bb221f8
Gavin (Zhengyu) Yang <yang.zhengy@northeastern.edu>
2022-11-28 12:30:23 -0800

    Save minor changes

--- app/src/main/java/edu/northeastern/elderberry/dayview/MedicationDayview.java
+++ app/src/main/java/edu/northeastern/elderberry/dayview/MedicationDayview.java
@@ -148,29 +148,6 @@ public class MedicationDayview extends AppCompatActivity {
 
         return ChildItemList;
 
-        // setting up cardView
-        //cardView = findViewById(R.id.base_cardview);
-        //arrow = findViewById(R.id.arrow_button);
-        //hiddenView = findViewById(R.id.hidden_view);
-
-        //arrow.setOnClickListener(view -> {
-        //    // If the CardView is already expanded, set its visibility
-        //    // to gone and change the expand less icon to expand more.
-        //    if (hiddenView.getVisibility() == View.VISIBLE) {
-        //        // The transition of the hiddenView is carried out by the TransitionManager class.
-        //        // Here we use an object of the AutoTransition Class to create a default transition
-        //        TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
-        //        hiddenView.setVisibility(View.GONE);
-        //        arrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
-        //    }
-
-        //    // If the CardView is not expanded, set its visibility to
-        //    // visible and change the expand more icon to expand less.
-        //    else {
-        //        TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
-        //        hiddenView.setVisibility(View.VISIBLE);
-        //    }
-        //});
     }
 
     // Method to inflate the options menu when the user opens the menu for the first time.
