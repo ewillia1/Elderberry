959fb60a0d0469c2a7a86c5a39ad3e6f530da53a
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2022-12-08 18:26:50 -0500

    Working on saving checkbox value -- however I am going to go back to the code before this. I am just pushing this just in case I need to go back and see what I have done.

--- app/src/main/java/edu/northeastern/elderberry/dayview/ChildItem.java
+++ app/src/main/java/edu/northeastern/elderberry/dayview/ChildItem.java
@@ -1,27 +1,38 @@
 package edu.northeastern.elderberry.dayview;
 
 import android.util.Log;
+import android.widget.CheckBox;
 
 public class ChildItem {
     private static final String TAG = "ChildItem";
 
     // Declaration of the variable
-    private String ChildItemTitle;
+    private String childItemTitle;
+    private CheckBox checkBox;
 
     // Constructor of the class to initialize the variable*
-    public ChildItem(String childItemTitle) {
+    public ChildItem(String childItemTitle,CheckBox checkBox) {
         Log.d(TAG, "_____ChildItem");
-        this.ChildItemTitle = childItemTitle;
+        this.childItemTitle = childItemTitle;
+        this.checkBox = checkBox;
     }
 
     // Getter and Setter method for the parameter
     public String getChildItemTitle() {
         Log.d(TAG, "_____getChildItemTitle");
-        return ChildItemTitle;
+        return childItemTitle;
+    }
+
+    public CheckBox getCheckBox() {
+        return this.checkBox;
     }
 
     public void setChildItemTitle(String childItemTitle) {
         Log.d(TAG, "_____setChildItemTitle");
-        ChildItemTitle = childItemTitle;
+        this.childItemTitle = childItemTitle;
+    }
+
+    public void setCheckBox(CheckBox checkBox) {
+        this.checkBox = checkBox;
     }
 }
